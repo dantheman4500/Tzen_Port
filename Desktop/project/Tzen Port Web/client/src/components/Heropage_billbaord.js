5d4c2c76a5a66c8d02dbbc8f8dc5c1a0e6cdabb5
dantheman4500 <danielromero4500@gmail.com>
2023-01-15 16:49:44 -0800

    I did a thing 2

---
+++ Desktop/project/Tzen Port Web/client/src/components/Heropage_billbaord.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+function billBoard() {
+    return (
+        <div className = 'main-container'>
+        <div className = 'static-container'>
+            <h1 className = 'static'> Hi, I am </h1>
+        </div>
+        
+        <div className = 'moving-text-container'>
+            <div className = 'moving-text'>
+                <p> Christopher </p>
+                <p> a photographer </p>
+                <p> a golfer </p>
+                <p> learning web </p>
+            </div>
+        </div>
+        </div>
+        );
+  }
+
+  export default billBoard;
